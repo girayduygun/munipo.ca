5830192480ab61aebc221179065e8be40c7f6a79
Giray Duygun <145222042+girayduygun@users.noreply.github.com>
2024-09-05 21:09:47 -0400

    Update and rename destinations.css to words.js

---
+++ words.js
@@ -0,0 +1,12 @@
+// List of words with Turkish and English meanings, and source information
+const words = [
+    {french: "Mettre en valeur", turkish: "vurgulamak", english: "to highlight", source: "Campus 1/Unité 1"},
+    {french: "La rencontre", turkish: "buluşma, flört", english: "meeting, date", source: "Campus 1/Unité 2"},
+    {french: "éviter", turkish: "kaçınmak", english: "to avoid", source: "Campus 1/Unité 3"},
+    {french: "La répétition", turkish: "tekrar", english: "repetition", source: "Campus 1/Unité 4"},
+    {french: "Faire le point sur les problèmes", turkish: "sorunları değerlendirmek", english: "to take stock of the problems", source: "Campus 1/Unité 5"},
+    {french: "Accueillir", turkish: "hoş karşılamak, hoş geldin demek", english: "to welcome", source: "Campus 1/Unité 6"},
+    {french: "Le domicile", turkish: "yaşadığı yer, ev", english: "home, residence", source: "Campus 1/Unité 7"},
+    {french: "Un établissement", turkish: "kurum, kuruluş", english: "establishment, institution", source: "Campus 1/Unité 8"},
+    {french: "Scolaire", turkish: "okul", english: "school, academic", source: "Campus 1/Unité 9"}
+];
