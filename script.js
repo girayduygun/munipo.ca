7e9f15b312a4b181a837cc2a327d0733271f7885
Giray Duygun <145222042+girayduygun@users.noreply.github.com>
2024-09-05 21:09:13 -0400

    Update and rename global.css to script.js

---
+++ script.js
@@ -0,0 +1,58 @@
+let randomWord = null;
+
+function generateRandomWord() {
+    const randomIndex = Math.floor(Math.random() * words.length);
+    randomWord = words[randomIndex];
+
+    document.getElementById('word').innerHTML = randomWord.french;
+    document.getElementById('turkishMeaning').innerHTML = `Turkish: ${randomWord.turkish}`;
+    document.getElementById('englishMeaning').innerHTML = `English: ${randomWord.english}`;
+    document.getElementById('source').innerHTML = `Source: ${randomWord.source}`;
+
+    // Hide meanings and reset French word display
+    document.getElementById('turkishMeaning').style.display = "none";
+    document.getElementById('englishMeaning').style.display = "none";
+    document.getElementById('source').style.display = "none";
+    document.getElementById('word').style.display = "block";
+}
+
+function generateRandomMeaning() {
+    const randomIndex = Math.floor(Math.random() * words.length);
+    randomWord = words[randomIndex];
+
+    document.getElementById('word').innerHTML = randomWord.french;
+    document.getElementById('turkishMeaning').innerHTML = `Turkish: ${randomWord.turkish}`;
+    document.getElementById('englishMeaning').innerHTML = `English: ${randomWord.english}`;
+    document.getElementById('source').innerHTML = `Source: ${randomWord.source}`;
+
+    // Show meanings and hide French word initially
+    document.getElementById('turkishMeaning').style.display = "block";
+    document.getElementById('englishMeaning').style.display = "block";
+    document.getElementById('source').style.display = "block";
+    document.getElementById('word').style.display = "none";
+}
+
+function toggleMeanings() {
+    const turkishMeaning = document.getElementById('turkishMeaning');
+    const englishMeaning = document.getElementById('englishMeaning');
+    const source = document.getElementById('source');
+
+    if (turkishMeaning.style.display === "none" && englishMeaning.style.display === "none" && source.style.display === "none") {
+        turkishMeaning.style.display = "block";
+        englishMeaning.style.display = "block";
+        source.style.display = "block";
+    } else {
+        turkishMeaning.style.display = "none";
+        englishMeaning.style.display = "none";
+        source.style.display = "none";
+    }
+}
+
+function toggleFrench() {
+    const word = document.getElementById('word');
+    if (word.style.display === "none") {
+        word.style.display = "block";
+    } else {
+        word.style.display = "none";
+    }
+}
